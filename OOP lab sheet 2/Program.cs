2a3721776e83ab71851ece5214c8f36abfca047f
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-09-26 11:15:00 +0100

    filepath 11:14

--- OOP lab sheet 2/Program.cs
+++ OOP lab sheet 2/Program.cs
@@ -10,6 +10,59 @@ namespace OOP_lab_sheet_2
     {
         static void Main(string[] args)
         {
+            string filepath = @"C: \Users\S00223854\OneDrive - Atlantic TU\OOP\OOP lab sheet 2\results.txt";
+
+            try
+            {
+                string[] fileContents = file.ReadAllLines(filepath);
+
+                int totalPoints = 0;
+                int results = 0;
+                int points = 0;
+
+                for (int i = 0; i < fileContents.Length; i++)
+                {
+                    results = Convert.ToInt32(fileContents[i]);
+
+                    if (results >= 90)
+                    {
+                        points = 100;
+                    }
+                    else if (results >= 80)
+                    {
+                        points = 88;
+                    }
+                    else if (results >= 70)
+                    {
+                        points = 77;
+                    }
+                    else if (results >= 60)
+                    {
+                        points = 66;
+                    }
+                    else if (results >= 50)
+                    {
+                        points = 56;
+                    }
+                    else if (results >= 40)
+                    {
+                        points = 46;
+                    }
+                    else if (results >= 30)
+                    {
+                        points = 37;
+                    }
+                    else if (results <= 30)
+                    {
+                        points = 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
