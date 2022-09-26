6b8459cdadabf9ab5a2da3866d6fc31f7379afaa
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-09-26 12:02:31 +0100

    Ex2 finished 12:02

---
+++ Ex2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\S00223854\Downloads\results.txt";
+
+
+            try
+            {
+                string[] fileContents = File.ReadAllLines(filepath);
+
+                int totalPoints = 0;
+                int results = 0;
+                int points = 0;
+
+                foreach (string line in fileContents)
+                {
+                    results = Convert.ToInt32(line);
+
+                    if (results >= 90)
+                    {
+                        points = 100;
+                    }
+                    else if (results >= 80)
+                    {
+                        points = 88;
+                    }
+                    else if (results >= 70)
+                    {
+                        points = 77;
+                    }
+                    else if (results >= 60)
+                    {
+                        points = 66;
+                    }
+                    else if (results >= 50)
+                    {
+                        points = 56;
+                    }
+                    else if (results >= 40)
+                    {
+                        points = 46;
+                    }
+                    else if (results >= 30)
+                    {
+                        points = 37;
+                    }
+                    else if (results <= 30)
+                    {
+                        points = 0;
+                    }
+
+                    totalPoints += points;
+
+                }
+
+                File.AppendAllText(filepath, Environment.NewLine + "totalPoints: " + totalPoints.ToString());
+
+            }
+            catch (IOException io)
+            {
+                Console.WriteLine(io.Message);
+                //throw;
+            }
+        }
+    }
+}
