df225732fc85e9f286a4251a020d560c1911c74a
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-09-26 12:54:08 +0100

    Ex 5 started 12:53

---
+++ Ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userLevel;
+            int userID;
+            int userGrade;
+
+        }
+    }
+}
